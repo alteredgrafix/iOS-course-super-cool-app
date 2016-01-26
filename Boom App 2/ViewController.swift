2f8365c68cd55f2009a520476adec32f2adf54d7
Cristofer Ransdell <cransdell@Cristofers-MacBook-Pro.local>
2016-01-26 11:15:54 -0500

    added files for Boom App

--- Boom App 2/ViewController.swift
+++ Boom App 2/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var blueDot: UIImageView!
+    @IBOutlet weak var redDot: UIImageView!
+    @IBOutlet weak var hideBlueBtn: UIButton!
+    @IBOutlet weak var hideRedBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func makeBlueDotDisappear (sender:AnyObject){
+        blueDot.hidden = true
+        hideBlueBtn.hidden = true
+        redDot.hidden = false
+        hideRedBtn.hidden = false
+    }
+    @IBAction func makeRedDotDisappear (sender:AnyObject){
+        blueDot.hidden = false
+        hideBlueBtn.hidden = false
+        redDot.hidden = true
+        hideRedBtn.hidden = true
+    }
 
 }
 
